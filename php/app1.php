f0331de3333dfb4892e6ca40901990b12dbc6e4e
saccee <115699141+saccee@users.noreply.github.com>
2023-07-05 17:19:59 +0530

    Update app1.php

--- php/app1.php
+++ php/app1.php
@@ -8,14 +8,14 @@ $employment = $_POST['employment'];
 $experience = $_POST['experience'];
 
 //DATABASE CONNECTION
-$con = new mysqli('localhost', 'root', '', 'fooddomain');
+$con = new mysqli('139.84.139.83', 'sasi', '1nter%^Gl0be', 'fooddomain');
 if ($con->connect_error) {
   die('connection failed');
 } else {
   $stmt = $con->prepare("INSERT INTO application (name,email,phone,specialty,employment,experience) VALUES (?,?,?,?,?,?)");
   $stmt->bind_param("ssisss", $name, $email, $phone, $specialty, $employment, $experience);
   $stmt->execute();
-  echo "<script>window.location.href = 'home.php';</script>";
+  echo "<script>window.location.href = '../index.php';</script>";
   $stmt->close();
   $con->close();
 }
